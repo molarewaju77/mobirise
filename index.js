636f86c3637c1a388ef8e8056abb934c337d8958
molar241 <molarewaju241@gmail.comm>
2024-04-11 12:15:31 -0700

    thursday

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function changeBg(){
     var navbar = document.getElementById('navbar');
-    var scrollValue = window.scrollY;
+    var scrollValue = window.scrollY || windows.pageYOffset; 
     if(scrollValue < 20){
-        navbar.classList.remove('bgColor');
+        navbar.classList.remove('bgColor'); 
     } else{
         navbar.classList.add('bgColor')
     }
 }
-window.addEventListener('scroll' , changeBg)
+window.addEventListener('scroll' , changeBg);
+windows.addEventListener('touchmove' , changeBg);
