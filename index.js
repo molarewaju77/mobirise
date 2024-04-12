89acd0d8c2fde5f12fb1faf4a80586ea32466f3c
molar241 <molarewaju241@gmail.comm>
2024-04-12 00:00:19 -0700

    friday

--- index.js
+++
@@ -1,9 +0,0 @@
-const navEl = document.querySelector('.navbar');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 20) {
-        navEl.classList.add('navbar-scrolled');}
-        else if (window.scrollY <= 30){
-            navEl.classList.remove('navbar-scrolled');
-        }
-});
